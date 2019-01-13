b69fa4e070882795ec9109fbae22de5c7d630554
rcideos <46614148+rcideos@users.noreply.github.com>
2019-01-12 22:53:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(function(req, res, next) { //allow cross origin requests
 }); 
 
 // configuration
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));               
 app.use('/public/uploads',express.static(__dirname + '/public/uploads'));
 
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                    
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
 app.get('/', function (req, res) {
   res.sendfile('./index.html')
